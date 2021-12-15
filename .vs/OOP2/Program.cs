c470ab4fb598f921c1d4a401b087a618db761439
Amalia Andreea Gurau <amalia.gurau@emmanuel.ro>
2021-12-15 17:52:00 +0200

    oop2

--- .vs/OOP2/Program.cs
+++ .vs/OOP2/Program.cs
@@ -6,7 +6,13 @@ namespace OOP2
     {
         static void Main(string[] args)
         {
-            
+            Facultate UEO = new Facultate("UEO");
+            UEO.Students = new Student[] { new Student("Petrila", "Damaris", new DateTime(2001, 08, 03), "123456789" ),
+                                           new Student("Daniel", "Andrei", new DateTime(2002, 01, 02), "987654321"),
+                                           new Student("Samuel", "Bodogan", new DateTime(2002, 08, 09), "91837465")};
+
+            Console.WriteLine(UEO.Index("123456789"));
+
         }
     }
 }
