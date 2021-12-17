764326304b94157cf6cf91dc55274511375e49df
Amalia Andreea Gurau <amalia.gurau@emmanuel.ro>
2021-12-17 08:15:00 +0200

    oop2

---
+++ OOP2/Facultate.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP2
+{
+    public class Facultate
+    {
+        public string UniName;
+        public Student[] Students;
+
+        public Facultate(string Name)
+        {
+            this.UniName = Name;
+        }
+
+        public string Index(string ID_student)
+        { 
+            for(int i=0; i<Students.Length; i++)
+            {
+                if(string.Equals(ID_student, Students[i].ID_student))
+                {
+                    string s = Students[i].firstName + " " + Students[i].lastName;
+                    return s;
+                }
+            }
+            return "Studentul nu a fost gasit";
+
+        }
+    }
+}
---
+++ OOP2/Student.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP2
+{
+    public class Student 
+    {
+        public string firstName;
+        public string lastName;
+        public DateTime dateOfBirth;
+        public string ID_student;
+
+        public Student(string FirstName, string LastName, DateTime dateOfBirth, string ID_student)
+        {
+            this.firstName = FirstName;
+            this.lastName = LastName;
+            this.dateOfBirth = dateOfBirth;
+            this.ID_student = ID_student;
+
+
+
+        }
+
+    } 
+}
